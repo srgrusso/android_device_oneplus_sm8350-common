4d1a3bce354efbac6ccad786b82e1f6a81f39a0a
srgrusso <srgrusso240@gmail.com>
2022-10-11 20:09:17 -0500

    sm8350-common: Ignore neverallows for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -171,6 +171,8 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
