chandu078/device_oneplus_sm8350-common

sm8350-common: Implement OnePlus Settings support
Taken from https://github.com/AOSiP-Devices/device_oneplus_sm8150-common/commits/eleven/DeviceSettings.
[cjh1249131356]:
- Fully refractor, cleanup duplicated codes everywhere.
- Write/Pick in more features.

Includes:
- Mute media volume on silent mode toggle
- HBM & Auto HBM + QS tile
- Ambient display + QS tile (merged oplusdoze into OnePlusSettings)
- FPS Info + QS tile
- Hyper Touch + QS tile
- Tp Edge Limit toggle (Unlock edge touch) + QS tile
- Vibrator strength preference
- CustomSeekbarPreference & CustomSwitchPreference (Include haptic feedback)

Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

OnePlusSettings/src/org/nameless/device/OnePlusSettings/Doze/PickupSensor.kt
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2021-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.device.OnePlusSettings.Doze
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.PowerManager
+import android.os.SystemClock
+import android.util.Log
+
+import java.util.concurrent.Executors
+
+import org.nameless.device.OnePlusSettings.Utils.DozeUtils
+
+class PickupSensor(
+    private val context: Context, sensorType: String, private val sensorValue: Float
+) : SensorEventListener {
+    private val powerManager = context.getSystemService(PowerManager::class.java)
+    private val wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG)
+
+    private val sensorManager = context.getSystemService(SensorManager::class.java)
+    private val sensor = DozeUtils.getSensor(sensorManager, sensorType)
+
+    private val executorService = Executors.newSingleThreadExecutor()
+    private var entryTimestamp = 0L
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (DEBUG) Log.d(TAG, "Got sensor event: ${event.values[0]}")
+        val delta = SystemClock.elapsedRealtime() - entryTimestamp
+        if (delta < MIN_PULSE_INTERVAL_MS) {
+            return
+        }
+        entryTimestamp = SystemClock.elapsedRealtime()
+        if (event.values[0] == sensorValue) {
+            if (DozeUtils.isPickUpSetToWake(context)) {
+                wakeLock.acquire(WAKELOCK_TIMEOUT_MS)
+                powerManager.wakeUpWithProximityCheck(
+                    SystemClock.uptimeMillis(), PowerManager.WAKE_REASON_GESTURE, TAG
+                )
+            } else {
+                DozeUtils.launchDozePulse(context)
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+    fun enable() {
+        if (sensor != null) {
+            Log.d(TAG, "Enabling")
+            executorService.submit {
+                entryTimestamp = SystemClock.elapsedRealtime()
+                sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+            }
+        }
+
+    }
+
+    fun disable() {
+        if (sensor != null) {
+            Log.d(TAG, "Disabling")
+            executorService.submit {
+                sensorManager.unregisterListener(this, sensor)
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "PickupSensor"
+        private const val DEBUG = false
+
+        private const val MIN_PULSE_INTERVAL_MS = 2500L
+        private const val WAKELOCK_TIMEOUT_MS = 300L
+    }
+}

srgrusso/android_device_oneplus_sm8350-common

OnePlusSettings/src/org/nameless/device/OnePlusSettings/Doze/PickupSensor.kt
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2021-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.device.OnePlusSettings.Doze
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.PowerManager
+import android.os.SystemClock
+import android.util.Log
+
+import java.util.concurrent.Executors
+
+import org.nameless.device.OnePlusSettings.Utils.DozeUtils
+
+class PickupSensor(
+    private val context: Context, sensorType: String, private val sensorValue: Float
+) : SensorEventListener {
+    private val powerManager = context.getSystemService(PowerManager::class.java)
+    private val wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG)
+
+    private val sensorManager = context.getSystemService(SensorManager::class.java)
+    private val sensor = DozeUtils.getSensor(sensorManager, sensorType)
+
+    private val executorService = Executors.newSingleThreadExecutor()
+    private var entryTimestamp = 0L
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (DEBUG) Log.d(TAG, "Got sensor event: ${event.values[0]}")
+        val delta = SystemClock.elapsedRealtime() - entryTimestamp
+        if (delta < MIN_PULSE_INTERVAL_MS) {
+            return
+        }
+        entryTimestamp = SystemClock.elapsedRealtime()
+        if (event.values[0] == sensorValue) {
+            if (DozeUtils.isPickUpSetToWake(context)) {
+                wakeLock.acquire(WAKELOCK_TIMEOUT_MS)
+                powerManager.wakeUp(
+                    SystemClock.uptimeMillis(), PowerManager.WAKE_REASON_GESTURE, TAG
+                )
+            } else {
+                DozeUtils.launchDozePulse(context)
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+    fun enable() {
+        if (sensor != null) {
+            Log.d(TAG, "Enabling")
+            executorService.submit {
+                entryTimestamp = SystemClock.elapsedRealtime()
+                sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+            }
+        }
+
+    }
+
+    fun disable() {
+        if (sensor != null) {
+            Log.d(TAG, "Disabling")
+            executorService.submit {
+                sensorManager.unregisterListener(this, sensor)
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "PickupSensor"
+        private const val DEBUG = false
+
+        private const val MIN_PULSE_INTERVAL_MS = 2500L
+        private const val WAKELOCK_TIMEOUT_MS = 300L
+    }
+}
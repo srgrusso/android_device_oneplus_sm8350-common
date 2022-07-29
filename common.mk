533e9fe159876f70d739fdbb24ff399224f21028
maxwen <max.weninger@gmail.com>
2022-07-29 14:20:40 +0000

    sm8350-common: Implement OnePlus Settings support
    Taken from https://github.com/AOSiP-Devices/device_oneplus_sm8150-common/commits/eleven/DeviceSettings.
    [cjh1249131356]:
    - Fully refractor, cleanup duplicated codes everywhere.
    - Write/Pick in more features.
    
    Includes:
    - Mute media volume on silent mode toggle
    - HBM & Auto HBM + QS tile
    - Ambient display + QS tile (merged oplusdoze into OnePlusSettings)
    - FPS Info + QS tile
    - Hyper Touch + QS tile
    - Tp Edge Limit toggle (Unlock edge touch) + QS tile
    - Vibrator strength preference
    - CustomSeekbarPreference & CustomSwitchPreference (Include haptic feedback)
    
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -30,7 +30,6 @@ PRODUCT_PACKAGES += \
 
 # Alert slider
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key-calibrate
 
 # Audio
@@ -154,10 +153,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-# Doze
-PRODUCT_PACKAGES += \
-    OplusDoze
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -328,6 +323,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     oplus-fwk.lahaina
+
+# OnePlusSettings
+PRODUCT_PACKAGES += \
+    OnePlusSettings
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
