3fad0c5f71e31f89ec4555cabde0fa78269097be
srgrusso <srgrusso240@gmail.com>
2023-03-19 12:19:18 -0500

    Blissify

---
+++ remove_packages/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+	Chrome \
+	Chrome-Stub \
+	Drive \
+	Maps \
+	Photos \
+	PixelLiveWallpaperPrebuilt \
+	PixelWallpapers2021 \
+	PrebuiltGmail \
+	WallpaperPickerGoogleRelease
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
