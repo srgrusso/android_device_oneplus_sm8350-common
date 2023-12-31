e027248d61729336f396665742fc55c2d48b61f6
mukesh22584 <mks22584@gmail.com>
2023-12-31 15:38:41 +0530

    sm8350-common: Update blobs from OxygenOS LE2121_14.0.0.500(EX01)

--- common.mk
+++ common.mk
@@ -115,6 +115,7 @@ TARGET_ENABLE_BLUR := true
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.stats@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.postproc@1.0.vendor
@@ -133,6 +134,7 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.common-V1-ndk.vendor \
     android.hardware.graphics.common-V1-ndk_platform.vendor \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
@@ -146,6 +148,7 @@ PRODUCT_PACKAGES += \
     libsdmcore \
     libsdmutils \
     libtinyxml \
+    libtinyxml2 \
     memtrack.default \
     vendor.display.config@1.0 \
     vendor.display.config@1.15.vendor \
